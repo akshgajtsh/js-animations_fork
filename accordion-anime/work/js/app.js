5117d2b8fbb4ecef8de7d5927ddaf07f79254cbe
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2026-05-29 22:57:02 +0900

    add: acordion_animation fin

---
+++ accordion-anime/work/js/app.js
@@ -0,0 +1,13 @@
+const accordions_animation = document.querySelectorAll('.list-item');
+accordions_animation.forEach(accordion_animarion => {
+    accordion_animarion.addEventListener('click', (event) => {
+        const text = accordion_animarion.querySelector('.list-content');
+        if (text.classList.contains('open')) {
+            text.classList.remove('open');
+            text.style.height = text.scrollHeight + 'px';
+        } else {
+            text.classList.add('open');
+            text.style.height = '0px';
+        }
+    });
+});
